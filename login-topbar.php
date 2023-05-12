9dea7fb83ca0712f5b37702dbff11c8e6855f9d3
EliasWal <96151440+EliasWal@users.noreply.github.com>
2023-05-12 21:16:12 +0300

    Add files via upload

---
+++ login-topbar.php
@@ -0,0 +1,33 @@
+<head>
+    <link href="../admin-topbar.css" rel="stylesheet" />
+    <link rel="icon" type="image/png" href="https://cdn-icons-png.flaticon.com/512/2102/2102805.png"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" /></head>
+</head>
+<header>
+        <nav>
+            <ul class='nav-bar'>
+                <div class='logo'>
+                        <img src='../photos/Baby manager.png'/> 
+                        <span class='logo-name'>BABY MANAGER</span>
+                  </div>
+                <input type='checkbox' id='check' />
+                <span class="menu">
+                    <li><a href="../Memories.html">Memories</a></li>
+                    <li><a href="../Calendars.html">Calendars</a></li>
+                    <li><a href="../friends.html">Friends</a></li>
+                    <li><a href="../Medical.html">Medical</a></li>
+                    <li><a href="../Welcome-logged-in.html">Home</a></li>
+                    <li><a href="../media.html">Media</a></li>
+                    <li class='settings'>
+                        <a href='settings.php'>
+                            <img src='../Photos/settings.png'/>
+                        </a>
+                    </li>
+                    <a href="../Welcome.html" >
+                    <button class="logout"> Log out </button></a>
+                    <label for="check" class="close-menu"><i class="fas fa-times"></i></label>
+                </span>
+                <label for="check" class="open-menu"><i class="fas fa-bars"></i></label>
+            </ul>
+        </nav>
+        </header>
