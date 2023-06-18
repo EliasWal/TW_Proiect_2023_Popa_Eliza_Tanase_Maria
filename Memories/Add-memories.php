be782514465ef90cf38596a8bc804a2127f2d9de
TANASE Maria <100341937+mariatanase@users.noreply.github.com>
2023-06-18 21:24:00 +0300

    resolve responsiveness issue

--- Memories/Add-memories.php
+++ Memories/Add-memories.php
@@ -21,7 +21,7 @@ $id_child = $_GET['id'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Baby manager </title>
     <link href="../admin-topbar.css" rel="stylesheet" />
-    <link href="Add-memories-style.css" rel="stylesheet" />
+    <link href="Add-memories-style1.css" rel="stylesheet" />
     <link rel="icon" type="image/png" href="https://cdn-icons-png.flaticon.com/512/2102/2102805.png"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" /></head>
     <script>
