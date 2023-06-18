6f5b7d36ed36c148183ffae02e84f4d2b49b656d
Elias <96151440+EliasWal@users.noreply.github.com>
2023-06-18 14:22:04 +0300

    wrong commit for media repaired

--- Media/upload-media.php
+++ Media/upload-media.php
@@ -29,13 +29,17 @@ $user_id = $_SESSION["id"];
         var form = document.getElementById('upload-form');
         var button = document.getElementById('myButton');
         var messageContainer = document.getElementById('message-container');
-        var userId = "<?php echo $user_id; ?>"; 
+        var userId = "<?php echo $user_id; ?>"; // Retrieve the user ID from PHP and assign it to a JavaScript variable
 
         button.addEventListener('click', function(event) {
-          event.preventDefault(); 
+          event.preventDefault(); // Prevent the default form submission
 
+          // Create a new FormData object
           var formData = new FormData(form);
+          
+        
 
+          // Perform the AJAX request
           var xhr = new XMLHttpRequest();
           xhr.open('POST', 'http://localhost/TW_Proiect_2023_Popa_Eliza_Tanase_Maria-main/api/media/');
           xhr.onload = function() {
@@ -50,12 +54,14 @@ $user_id = $_SESSION["id"];
           };
           xhr.onerror = function() {
             console.log('Request error');
+            // Handle the request error
           };
           xhr.send(formData);
         });
 
         function showMessage(message) {
           messageContainer.textContent = message;
+          // You can also style the message container or perform other actions here
         }
       });
     </script>
